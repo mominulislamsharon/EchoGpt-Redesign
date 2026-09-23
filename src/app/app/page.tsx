bfd6eef88e192579180c5e34be7bde8068e4c571
mominulislamsharon <islamikchannel328250@gmail.com>
2026-09-24 03:59:28 +0600

    initialize Next.js project with folder structure

---
+++ src/app/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      Redesigned web app
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/extension/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      Extension page
+    </div>
+  );
+};
+
+export default page;
